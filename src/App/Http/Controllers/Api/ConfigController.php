0d2b2c67ffe6de4f2079a047dd7cb1ac58f5151c
bigrocs <bigrocs@qq.com>
2017-09-19 16:02:35 +0800

    控制器添加内容

---
+++ src/App/Http/Controllers/Api/ConfigController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CoreCMF\Omnipay\App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ConfigController extends Controller
+{
+    public function __construct(){
+    }
+    public function index()
+    {
+
+        return [];
+    }
+}
---
+++ src/App/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CoreCMF\Omnipay\App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    public function __construct(){
+    }
+    public function index()
+    {
+
+        return [];
+    }
+}
