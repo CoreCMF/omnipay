3cdb703030f4f25558e0948226e12f27309ea61b
bigrocs <bigrocs@qq.com>
2017-09-23 17:27:17 +0800

    增加迁移文件

--- src/Databases/migrations/2017_09_01_024032_omnipay_configs_table.php
+++ src/Databases/migrations/2017_09_01_024032_omnipay_configs_table.php
@@ -15,6 +15,14 @@ class OmnipayConfigsTable extends Migration
     {
         Schema::create('omnipay_configs', function (Blueprint $table) {
             $table->increments('id')->unsigned();
+            $table->string('driver',30)         ->comment('驱动');
+            $table->string('appId',60)          ->comment('AppId') ->nullable();
+            $table->string('sellerID',80)       ->comment('商家ID')->nullable();
+            $table->string('returnUrl',160)     ->comment('回调URl')->nullable();
+            $table->string('notifyUrl',160)     ->comment('通知URl')->nullable();
+            $table->string('privateKey',80)     ->comment('密钥文件路径')->nullable();
+            $table->string('publicKey',80)      ->comment('公钥文件路径')->nullable();
+            $table->string('certPassword',80)   ->comment('银联商家密钥文件密码')->nullable();
             $table->boolean('status')           ->comment('开关状态')->default(false);
             $table->timestamps();
         });
