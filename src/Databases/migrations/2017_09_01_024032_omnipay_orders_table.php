0f081ae7620f5863860859398e28f70d808bb57f
bigrocs <bigrocs@qq.com>
2017-09-29 16:47:36 +0800

    增加订单名称

--- src/Databases/migrations/2017_09_01_024032_omnipay_orders_table.php
+++ src/Databases/migrations/2017_09_01_024032_omnipay_orders_table.php
@@ -16,6 +16,7 @@ class OmnipayOrdersTable extends Migration
         Schema::create('omnipay_orders', function (Blueprint $table) {
             $table->string('id')                 ->comment('订单编号')->unique();
             $table->bigInteger('uid')            ->comment('用户ID')->default(0)->unsigned();
+            $table->string('name',275)           ->comment('订单名称');
             $table->decimal('fee',11, 2)         ->comment('订单金额')->unsigned()->default(0);
             $table->string('gateway',30)         ->comment('支付网关');
             $table->string('query_id')           ->comment('网关订单编号')->nullable();
