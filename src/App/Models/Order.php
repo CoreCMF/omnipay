70e34084c6ec45d7bbc96c0b087e790e2f4f50ba
bigrocs <bigrocs@qq.com>
2017-09-29 16:47:47 +0800

    增加Order模型

---
+++ src/App/Models/Order.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CoreCMF\Omnipay\App\Models;
+
+use Schema;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public $table = 'omnipay_orders';
+
+    protected $fillable = [];
+    
+}
