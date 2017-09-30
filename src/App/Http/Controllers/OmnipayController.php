98e5dd407b49d795155d233e4d52ce77c50c936c
bigrocs <bigrocs@qq.com>
2017-09-30 11:04:12 +0800

    数据整理

--- src/App/Http/Controllers/OmnipayController.php
+++ src/App/Http/Controllers/OmnipayController.php
@@ -10,9 +10,11 @@ use CoreCMF\Omnipay\App\Models\Order;
 class OmnipayController extends Controller
 {
     private $orderModel;
+    private $request;
 
-    public function __construct(Order $orderPro){
+    public function __construct(Order $orderPro, Request $request){
        $this->orderModel = $orderPro;
+       $this->request = $request;
     }
     public function pay($gatewayNmae)
     {
@@ -25,8 +27,8 @@ class OmnipayController extends Controller
         // ];
         // $order = $this->orderModel->create($createOrder);//订单写入数据库
 
-        $order_id = '20170929090814572607';
-        $order = $this->orderModel->where('order_id', $order_id)->first();
+        $orderId = '20170929090814572608';
+        $order = $this->orderModel->getOrder($orderId);
         $gateway = resolve('omnipay')->gateway($gatewayNmae);
         switch ($gatewayNmae) {
           case 'alipay':
@@ -96,41 +98,64 @@ class OmnipayController extends Controller
     /**
      * [callback 回调处理]
      * @param  [type]   $gatewayNmae [description]
-     * @param  Request  $request [description]
      * @return function          [description]
      */
-    public function callback($gatewayNmae, Request $request)
+    public function callback($gatewayNmae)
     {
-        $this->completePurchase($gatewayNmae, $request);
+        dd($this->completePurchase($gatewayNmae));
     }
     /**
      * [notify 异步通知处理]
      * @param  [type]  $gatewayNmae [description]
-     * @param  Request $request [description]
      * @return [type]           [description]
      */
-    public function notify($gatewayNmae, Request $request)
+    public function notify($gatewayNmae)
     {
-        $this->completePurchase($gatewayNmae, $request);
+        $this->completePurchase($gatewayNmae);
     }
-    protected function completePurchase($gatewayNmae, $request)
+    /**
+     * [completePurchase 处理支付返回数据]
+     * @param  [type] $gatewayNmae [description]
+     * @return [type]              [description]
+     */
+    protected function completePurchase($gatewayNmae)
     {
         $gateway = resolve('omnipay')->gateway($gatewayNmae);
-        switch ($gatewayNmae) {
-          case 'alipay':
-            $options = [
-                'params' => $request->all()
-            ];
-            break;
-          default:
-            $options = [
-                'request_params' => $request->all()
-            ];
-            break;
-        }
+        $options = [
+            'params' => $this->request->all(),
+            'request_params' => $this->request->all()
+        ];
         $response = $gateway->completePurchase($options)->send();
-        dd($response);
-        $response =get_class_methods($response);
-        dd($response);
+        if ($response->isPaid()||$response->isSuccessful()) {
+            $data = $response->getData();
+            switch ($gatewayNmae) {
+              case 'alipay':
+                $order = [
+                    'order_id'  => $data['out_trade_no'],
+                    'fee'       => $data['total_amount'],
+                    'query_id'  => $data['trade_no'],
+                ];
+                break;
+              case 'wechat':
+                $order = [
+                    'order_id'  => $data['out_trade_no'],
+                    'fee'       => $data['total_amount'],
+                    'query_id'  => $data['trade_no'],
+                ];
+                break;
+              case 'unionpay':
+                $order = [
+                    'order_id'  => $data['orderId'],
+                    'fee'       => $data['txnAmt']*0.01,
+                    'query_id'  => $data['queryId'],
+                ];
+                break;
+            }
+            $order['gateway'] = $gatewayNmae;
+            $order['status'] = 'paid';
+            return $this->orderModel->paySuccess($order);
+        }else{
+            return false;
+        }
     }
 }
