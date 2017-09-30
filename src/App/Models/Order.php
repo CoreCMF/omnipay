9d070147314f41ac0e883c31cbb38b59fae54ab7
bigrocs <bigrocs@qq.com>
2017-09-30 11:03:54 +0800

    设置订单数据  支付成功后数据处理

--- src/App/Models/Order.php
+++ src/App/Models/Order.php
@@ -11,4 +11,38 @@ class Order extends Model
 
     protected $fillable = ['order_id','name','fee','gateway'];
 
+    public $order;
+
+    /**
+     * [getOrder 根据订单id获取订单]
+     * @param  [type] $orderId [description]
+     * @return [type]          [description]
+     */
+    public function getOrder($orderId)
+    {
+        return $this->where('order_id', $orderId)->first();
+    }
+    /**
+     * [paySuccess 支付成功后处理]
+     * @param  [type] $completeOrder [description]
+     * @return [type]                [description]
+     */
+    public function paySuccess($completeOrder)
+    {
+        $update = $this->where('order_id', $completeOrder['order_id'])->where('fee', $completeOrder['fee'])->update($completeOrder);
+        if ($update) {
+            $order = $this->where('order_id', $completeOrder['order_id'])->where('fee', $completeOrder['fee'])->first();
+            $this->setOrder($order);
+            // event(new Order($this)); //支付完成事件
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function setOrder($order)
+    {
+        $this->order = $order;
+        return $this;
+    }
 }
